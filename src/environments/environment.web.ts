c83657fdf1511d63464146ca9a717e3de2ad20a5
Alban Tiberghien <atiberghien@numnprod.fr>
2023-12-01 16:49:50 +0100

    Ready to prod

---
+++ src/environments/environment.web.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serverHost: 'https://app.lamaisonreconnectee.fr',
+  isStandalone: false,
+  houseless: true,
+  apiKey: 'lBE0XaBG.QMGMvhokkKLwSuQ7WH4dioaFQiyzu545',
+};

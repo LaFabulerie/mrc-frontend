96528907b7494cb837da8ca2a693e416aa2d9620
Alban Tiberghien <atiberghien@numnprod.fr>
2023-12-08 15:59:41 +0100

    Minor comment fix

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverHost: 'http://<BACKEND_IP_TO_COMPLETE>:8080',
-  isStandalone: true, //if true, the app will run in web mode and apiKey must be set
+  isStandalone: true, //if false, the app will run in web mode and apiKey must be set
   houseless: false, //can be set to true if this app is standalone
   apiKey: null,
 };

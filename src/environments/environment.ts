0ff330cbe570c67b5b6319b4f788bb3189e3a054
Mindiell <mindiell@mindiell.net>
2024-02-16 10:59:09 +0100

    Suppression du fichier de configuration 'par défaut'

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  serverHost: 'http://172.20.93.115:8080',
-  isStandalone: true, //if false, the app will run in web mode and apiKey must be set
-  houseless: true, //can be set to true if this app is standalone
-  apiKey: null,
-};

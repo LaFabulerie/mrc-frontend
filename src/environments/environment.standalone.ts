ecb611a72515b4cb6c4845cd30b6225bd36c3716
Mindiell <mindiell@mindiell.net>
2024-02-16 14:41:32 +0100

    Ajout de la configuration locale

---
+++ src/environments/environment.standalone.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  serverHost: 'http://localhost:8000',
+  isStandalone: false, //if false, the app will run in web mode and apiKey must be set
+  houseless: true, //can be set to true if this app is standalone
+  isPrintable: true, //if true, the app will be able to print basket
+  apiKey: null,
+};

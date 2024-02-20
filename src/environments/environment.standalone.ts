228f5f005ea5352961e599b93a9def6d2255ac9c
Mindiell <mindiell@mindiell.net>
2024-02-20 16:16:45 +0100

    Accès corrigé au back pour une installation en standalone

--- src/environments/environment.standalone.ts
+++ src/environments/environment.standalone.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverHost: 'http://localhost:8000',
+  serverHost: 'http://localhost:8080',
   isStandalone: false, //if false, the app will run in web mode and apiKey must be set
   houseless: true, //can be set to true if this app is standalone
   isPrintable: true, //if true, the app will be able to print basket

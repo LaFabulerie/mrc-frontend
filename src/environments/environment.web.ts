b94b8b8223d90f039005e6ef43b579ebbe2cf5b0
Mindiell <github@mindiell.net>
2024-06-17 19:07:29 +0200

    Positionnement sur l'application officielle

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  serverHost: 'http://tback.lamaisonreconnectee.fr',
+  serverHost: 'http://back.lamaisonreconnectee.fr',
   isStandalone: false,
   houseless: true,
   isPrintable: false, //if true, the app will be able to print basket
-  apiKey: 'lBE0XaBG.QMGMvhokkKLwSuQ7WH4dioaFQiyzu545',
+  apiKey: '',
 };

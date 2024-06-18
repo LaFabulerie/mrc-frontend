707da816e4208095f6bad196bad23729c5857982
Mindiell <github@mindiell.net>
2024-06-18 17:59:16 +0200

    Mise à jour pour le https

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverHost: 'http://back.lamaisonreconnectee.fr',
+  serverHost: 'https://back.lamaisonreconnectee.fr',
   isStandalone: false,
   houseless: true,
   isPrintable: false, //if true, the app will be able to print basket

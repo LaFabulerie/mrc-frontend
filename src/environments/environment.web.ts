9ab33645bdc4131d8b224da27fd262ad4223cff2
Mindiell <github@mindiell.net>
2024-06-20 15:19:09 +0200

    Retrait du https temporaire

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverHost: 'https://back.lamaisonreconnectee.fr',
+  serverHost: 'http://back.lamaisonreconnectee.fr',
   isStandalone: false,
   houseless: true,
   isPrintable: false, //if true, the app will be able to print basket
